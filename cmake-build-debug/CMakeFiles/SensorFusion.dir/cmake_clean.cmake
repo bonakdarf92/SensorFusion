b4789d5124180547c474df4fae10f848e994c4bf
bonakdarf92 <bonakdarf92@hotmail.de>
2020-12-19 21:01:51 +0100

    First Commit Lidar

---
+++ cmake-build-debug/CMakeFiles/SensorFusion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SensorFusion.dir/main.cpp.o"
+  "SensorFusion.pdb"
+  "SensorFusion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SensorFusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
