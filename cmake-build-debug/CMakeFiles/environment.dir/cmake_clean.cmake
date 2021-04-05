9ca275786489b33a46b0421db3ca5b3a70fbb1c4
fb <fb@solidat.de>
2021-04-05 13:47:06 +0200

    SensorFusion: Fix update 2

--- cmake-build-debug/CMakeFiles/environment.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/environment.dir/cmake_clean.cmake
@@ -1,10 +1,11 @@
 file(REMOVE_RECURSE
   "CMakeFiles/environment.dir/src/environment.cpp.o"
-  "CMakeFiles/environment.dir/src/render/render.cpp.o"
   "CMakeFiles/environment.dir/src/processPointClouds.cpp.o"
+  "CMakeFiles/environment.dir/src/quiz/cluster/cluster.cpp.o"
   "CMakeFiles/environment.dir/src/quiz/ransac/ransac.cpp.o"
-  "environment.pdb"
+  "CMakeFiles/environment.dir/src/render/render.cpp.o"
   "environment"
+  "environment.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
